0099719ae87dd62b8a14e39f9e3abaddab1aa627
Mark <marklajom.work@gmail.com>
2023-11-14 11:38:31 +0100

    added setter prop to star rating component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
 // import "./index.css";
@@ -7,7 +7,25 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Test />
     {/* <App /> */}
     <StarRating maxRating={10} />
+    <StarRating size={30} color="red" />
   </React.StrictMode>
 );
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <>
+      <StarRating
+        color="blue"
+        size={40}
+        maxRating={10}
+        onSet={setMovieRating}
+      />
+      <h1>The movie is rated {movieRating}</h1>
+    </>
+  );
+}
