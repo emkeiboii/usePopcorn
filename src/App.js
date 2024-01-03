468b02b3d157a045e57e813abb5df4dd26a2cd6a
Mark <marklajom.work@gmail.com>
2024-01-03 11:47:46 +0100

    cleanup after data using abortcontroller

--- src/App.js
+++ src/App.js
@@ -34,12 +34,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) throw new Error("Something went wrong with fetching");
@@ -49,9 +52,12 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -62,6 +68,9 @@ export default function App() {
         }
       }
       fetchMovies();
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
