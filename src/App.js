672d3c97ced2ff4e2a6ec955754abbeec30ba235
Mark <marklajom.work@gmail.com>
2024-01-03 15:48:20 +0100

    enabled dom to listen to esc key to close movie details

--- src/App.js
+++ src/App.js
@@ -276,6 +276,22 @@ function MovieDetails({
     [title]
   );
 
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
